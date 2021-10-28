28a69017ad010889d3a103dc5a9a05bd436ad691
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-10-28 17:36:52 -0300

    setup

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Olá mundo! Este é o app cjfinance do P2 de TecWeb!")
