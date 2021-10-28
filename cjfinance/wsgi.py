5bbeb9a0bbc76f3fc057c3fc46197bafd4cec5ac
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-10-28 16:30:55 -0300

    esqueleto do django

---
+++ cjfinance/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cjfinance project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cjfinance.settings')
+
+application = get_wsgi_application()
