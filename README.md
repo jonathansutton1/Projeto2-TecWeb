ab46c83b5bc4342bc7f236e55ec80b9ff06e7352
Cesar0106 <cesarea@al.insper.edu.br>
2021-11-04 17:43:05 -0300

    Estrutura feita mas incompleta

--- README.md
+++
@@ -1,5 +0,0 @@
-# Projeto 2 da disciplina de Tecnologias Web
-
-**Desenvolvido por:**
-- Cesar Adees
-- Jonathan Sutton
--- notes/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Olá mundo! Este é o app cjfinance do P2 de TecWeb!")
